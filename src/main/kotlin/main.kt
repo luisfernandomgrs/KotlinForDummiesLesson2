68629135d46e0190ad98c594bc3e79e3d1692a18
Luis Fernando Machado <>
2022-12-21 23:58:44 -0300

    Repetition Structure Static, For

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
 
     structure_while()
     structure_while_2()
+    structure_for()
 }
 
 fun structure_while() {
@@ -9,6 +10,7 @@ fun structure_while() {
     var aVector = Array(10, {i -> i * i})
     var index = 0
 
+    println("Structure While, stactic")
     while (index < 8) {
         println(aVector[index])
         index++
@@ -21,9 +23,20 @@ fun structure_while_2() {
     var aVector = Array(10, {i -> i * i})
     var index = 0
 
+    println("Structure While, dynamic")
     // size count dynamic
     while (index < aVector.size) {
         println(aVector[index])
         index++
+    }
+}
+
+fun structure_for() {
+    // using lambda code
+    var aVector = Array(10, {i -> i * i})
+
+    println("Structure, For")
+    for (index in 0..9) {
+        println(aVector[index])
     }
 }
